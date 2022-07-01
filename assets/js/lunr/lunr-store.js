d6f1ced8ea7a490f40307d1d3d59b76b5e38e8a4
dlfelps <dlfelps@users.noreply.github.com>
2022-07-01 21:28:04 +0000

    jekyll build from Action e05402db2018fd875abfca26234af10dea97caa7

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Welcome to my blog!",
+        "excerpt":"My goal for this site is to document the “experiments” that I perform while exploring various computer science topics. I hope to include future posts on the following topics:      .NET   domain driven design   software architecture   If you have any suggestions or comments please email me at dlfelps@gmail.com.  ","categories": [],
+        "tags": [],
+        "url": "/2022/06/04/My-First-Post.html",
+        "teaser": null
+      },{
+        "title": "Experiment 01",
+        "excerpt":"This experiment explores various publishing options associated with .NET, including: framework-dependent vs self-contained Windows vs Linux vs OSX and more… Introduction The traditional way to distribute a .NET application is by compiling code (e.g. C#) into bytecode known as CIL. This bytecode can then be run by anyone who has...","categories": [],
+        "tags": ["net"],
+        "url": "/2022/06/06/DOTNET-Publishing-Options.html",
+        "teaser": null
+      },{
+        "title": "Experiment 02",
+        "excerpt":"The next few posts take step back to examine the benefits of creating reproducible software. We will explore: the continuosly reproducible mindset (this post) foundational tools for reproducibility (Exp 03) creating a continuously reproducible .NET project (Exp 04) Introduction Scientific experiments must be repeatable and reproducable to be considered scientific....","categories": [],
+        "tags": ["reproducible","net"],
+        "url": "/2022/06/20/Reproducible-Dotnet-Series.html",
+        "teaser": null
+      },{
+        "title": "Experiment 03",
+        "excerpt":"This post introduces the tools that I use to make my code reproducible. NOTE: I have only tested this on smallish projects and there certainly are other ways to create reproducible software.   Introduction   T     code hosting   container technology   continuous integration / continuous delivery   non-technical considerations (licensing)    ","categories": [],
+        "tags": ["reproducible","net","docker"],
+        "url": "/2022/06/25/Reproducible-Foundations.html",
+        "teaser": null
+      }]
